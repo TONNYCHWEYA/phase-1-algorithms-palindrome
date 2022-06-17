67ea819ff61d2206ed6dce6e7180c7d9f39c07c8
TONNYCHWEYA <chweyatonny@gmail.com>
2022-06-17 20:54:16 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const smallWorld =word.toLowerCase()
+  const wordArray = word.split('')
+  const reverseWordArray = wordArray.reverse()
+  const reverseWord = reverseWordArray.join('')
+  if(smallWorld === reverseWord){ 
+    return true
+  }else{
+    return false
+  }
+
 }
 
-/* 
+/* )
   Add your pseudocode here
+  convert string to lower case
+   convert string to an array
+   reverse the elements of the array
+   convert the array back to string
+   compare the original string with the new string that has been converted to the array
+   if they are the same return true or else return false
+
+  
 */
 
 /*
   Add written explanation of your solution here
+  toLowerCase() method converts string to lower case
+  split() method converts to  array
+  the array is reversed using the reverse method
+  the reversed array was turned back to a string using reversed() method
 */
 
 // You can run `node index.js` to view these console logs
